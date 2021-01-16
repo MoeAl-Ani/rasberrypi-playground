8d92682188a4c36fb5f9efdbb90a7e08d34624fb
Mohammed Al-Ani <mohammedalanny@gmail.com>
2021-01-16 16:38:26 +0200

    - wip madness

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod led;
-use sensehat::{SenseHat, Colour};
 
 fn main() {
-    let hat = sensehat::SenseHat::new();
-    println!("{:?}", hat.get_pressure());
-    hat.text("Hi!", Colour::RED, Colour::WHITE);
+
+    let humidity = 0x5f;
+    unsafe {
+
+    }
 }
